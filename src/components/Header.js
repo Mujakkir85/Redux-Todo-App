e89504733209e58387f67090b1e832b93d8530dd
Mujakkir85 <mujakkirlucse@gmail.com>
2022-10-13 13:15:37 +0600

    add new item and header function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,43 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import tickImage from "../assets/images/double-tick.png";
 import noteImage from "../assets/images/notes.png";
 import plusImage from "../assets/images/plus.png";
+import { added, allCompleted, clearCompleted } from "../redux/todos/action";
 
 export default function Header() {
+
+    const dispatch = useDispatch();
+    const [input, setInput] = useState('');
+
+    const handleInput = (e) =>{
+        setInput(e.target.value)
+    }
+
+    const submitHandeler = (e) =>{
+        e.preventDefault();
+        dispatch(added(input));
+        setInput("")
+    }
+
+    const completeHandeler = () =>{
+        dispatch(allCompleted())
+    }
+
+    const clearHandeler = () => {
+        dispatch(clearCompleted())
+    }
+
     return (
         <div>
-            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md" onSubmit={submitHandeler}>
                 <img src={noteImage} className="w-6 h-6" alt="Add todo" />
                 <input
                     type="text"
                     placeholder="Type your todo"
                     className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+                    value={input}
+                    onChange={handleInput}
                 />
                 <button
                     type="submit"
@@ -19,11 +46,11 @@ export default function Header() {
             </form>
 
             <ul className="flex justify-between my-4 text-xs text-gray-500">
-                <li className="flex space-x-1 cursor-pointer">
+                <li className="flex space-x-1 cursor-pointer" onClick={completeHandeler}>
                     <img className="w-4 h-4" src={tickImage} alt="Complete" />
                     <span>Complete All Tasks</span>
                 </li>
-                <li className="cursor-pointer">Clear completed</li>
+                <li className="cursor-pointer"  onClick={clearHandeler}>Clear completed</li>
             </ul>
         </div>
     );
